d7f69aad007578fb3aa3836c3fc55236ee19dd49
Furriel <l.furriel35@gmail.com>
2023-11-06 17:27:21 -0300

    Finalizado split

--- teste.c
+++ teste.c
@@ -4,6 +4,7 @@
 int main () {
     FILE *fp;
 
+    remove("Files/ibtree.idx");
     if ((fp = fopen("Files/ibtree.idx", "r+")) == NULL) {
         fp = fopen("Files/ibtree.idx", "w+");
         escreve_pagina_vazia(fp);
@@ -11,12 +12,23 @@ int main () {
 
     int raiz = 0;
 
-    raiz = insere_chave(raiz, fp, "AAA11", 1);
-    raiz = insere_chave(raiz, fp, "BBB11", 2);
-    raiz = insere_chave(raiz, fp, "CCC11", 3);
-    raiz = insere_chave(raiz, fp, "DDD11", 4);
-    raiz = insere_chave(raiz, fp, "EEE11", 5);
+    raiz = insere_chave(raiz, fp, "BBB11", 2); //ok
+    raiz = insere_chave(raiz, fp, "AAA11", 1); //ok
+    raiz = insere_chave(raiz, fp, "CCC11", 3); //ok
+    raiz = insere_chave(raiz, fp, "DDD11", 4); //ok
+    raiz = insere_chave(raiz, fp, "EEE11", 5); //ok
+    raiz = insere_chave(raiz, fp, "FFF11", 6); //ok
+    raiz = insere_chave(raiz, fp, "GGG11", 7); //ok
+    raiz = insere_chave(raiz, fp, "HHH11", 8); //ok
+    raiz = insere_chave(raiz, fp, "III11", 9); //ok
+    raiz = insere_chave(raiz, fp, "JJJ11", 10); //ok
+    raiz = insere_chave(raiz, fp, "AAA13", 11); //ok
 
+    int busca = busca_registro(raiz, fp, "AAA13");
+    printf("%d\n", busca);
     fclose(fp);
     printf("%d", raiz);
+    scanf("%d", &raiz);
+
+    return 0;
 }
